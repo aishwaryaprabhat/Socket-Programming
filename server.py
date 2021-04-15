3277c82f7e3947f1711d0e561bf5cdd9e0ee0c47
Aishwarya Prabhat <aish.prabhat@shopee.com>
2021-04-15 15:08:44 +0800

    server done

---
+++ server.py
@@ -0,0 +1,44 @@
+import socket
+import threading
+
+HEADER = 64
+PORT = 5050
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!DISCONNECT"
+SERVER = "localhost" #socket.gethostbyname(socket.gethostname()) #get IP address by name
+# print(socket.gethostname())
+
+
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ADDR = (SERVER, PORT)
+server.bind(ADDR)
+
+
+def handle_client(conn, addr):
+    print(addr, " connected.")
+    connected = True
+    while connected:
+        #determining message length
+        msg_length = conn.recv(HEADER).decode(FORMAT)
+        msg_length = int(msg_length)
+
+        msg = conn.recv(msg_length).decode(FORMAT)
+        print(addr," said: ",msg)
+
+        #handling disconnection cleanly
+        if msg==DISCONNECT_MESSAGE:
+            connected = False
+
+
+def start():
+    server.listen()
+    print("Server is listening on ", SERVER)
+    while True:
+        conn, addr = server.accept()
+        thread = threading.Thread(target=handle_client, args=(conn, addr))
+        thread.start()
+        print("Acting connections: {}".format(threading.active_count()-1))
+
+print("Starting server...")
+start()
